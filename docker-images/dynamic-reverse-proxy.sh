81beb2f1213ee0f09c48639b26b8bc2e45165de6
Iando Rafid <iando.rafid@gmail.com>
2018-06-12 10:17:10 +0200

    update demo script

--- docker-images/dynamic-reverse-proxy.sh
+++ docker-images/dynamic-reverse-proxy.sh
@@ -28,14 +28,27 @@ do
 done
 
 docker run -d --name express_dynamic res/express
-
-
+docker run -d --name express_dynamic_2 res/express
 
 docker build -t res/apache_rp apache-reverse-proxy/.
-docker inspect apache_static | grep -i ipaddress
-docker inspect express_dynamic | grep -i ipaddress
+
+
+ip_static_9090=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' static-9090)
+ip_static_1010=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' static-1010)
+
+echo "static-9090 IP Adress: " $ip_static_9090
+echo "static-1010 IP Adress: " $ip_static_1010
+
 
 ip_static=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' apache_static)
 ip_dynamic=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' express_dynamic)
+ip_dynamic_2=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' express_dynamic_2)
+
+echo "apache_static IP Address: " $ip_static
+echo "express_dynamic: " $ip_dynamic
+echo "express_dynamic_2: " $ip_dynamic_2
 
 docker run -d -p 8080:80 -e STATIC_APP=$ip_static -e DYNAMIC_APP=$ip_dynamic --name apache_rp res/apache_rp
+
+docker ps -a
+docker images
